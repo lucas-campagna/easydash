5cd9d58923b7417141f7e1141eef66feae9091fc
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2022-10-31 22:50:26 -0300

    new default size

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 DEV = True
 
-window_default = dict(x=200,y=200,width=500,height=500)
+window_default = dict(x=100,y=100,width=300,height=200)
 
 dragGrid=[20,20]
 resizeGrid=[20,20]
 
+default_debug_msg = 'No problems have been detected in the workspace.'
+
+isDev = lambda : DEV
